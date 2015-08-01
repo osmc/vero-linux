fbcfeaea5e7f74d985f9a6708c0ac47a2db92280
Matus Kral <matuskral@me.com>
2015-08-01 03:24:15 +0200

    imx-edid: don't check fract modes bit and 3d modes bit. those are created and identified by the same VIC and find_nearest should find them.

--- drivers/video/mxc/mxc_edid.c
+++ drivers/video/mxc/mxc_edid.c
@@ -795,21 +795,11 @@ const struct fb_videomode *mxc_fb_find_nearest_mode(const struct fb_videomode *m
 		modelist = list_entry(pos, struct fb_modelist, list);
 		cmode = &modelist->mode;
 
-		if (!(mode->vmode & FB_VMODE_3D_MASK) &&
-		     (cmode->vmode & FB_VMODE_3D_MASK))
-			continue;
-		if ((mode->vmode & FB_VMODE_3D_MASK) &&
-		   ((mode->vmode & FB_VMODE_3D_MASK) != (cmode->vmode & FB_VMODE_3D_MASK)))
-			continue;
-
 		if ((mode->vmode & FB_VMODE_MASK_SIMPLE) != (cmode->vmode & FB_VMODE_MASK_SIMPLE))
 			continue;
 
 		if ((mode->vmode & FB_VMODE_ASPECT_MASK) &&
 		   ((mode->vmode & FB_VMODE_ASPECT_MASK) != (cmode->vmode & FB_VMODE_ASPECT_MASK)))
-			continue;
-
-		if ((mode->vmode & FB_VMODE_FRACTIONAL) != (cmode->vmode & FB_VMODE_FRACTIONAL))
 			continue;
 
 		d = abs(cmode->xres - mode->xres) +
