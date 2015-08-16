boundarydevices/linux

MLK-9996 arm: imx6: Correct the AHB clock in low_bus_freq_mode
When the busfreq is in audio_bus_freq_mode, the AHB bus is at 8MHz,
in low_bus_freq_mode, the AHB needs to run at 24MHz. So when switching
from audio_bus_freq_mode to low_bus_freq_mode, make sure the AHB is at
24MHz in low_bus_freq_mode.

Signed-off-by: Bai Ping <b51503@freescale.com>

arch/arm/mach-imx/busfreq-imx6.c
@@ -280,6 +280,12 @@ static void enter_lpm_imx6sl(void)
 		} else {
 			if (!ultra_low_bus_freq_mode && !low_bus_freq_mode) {
 				/*
+				 * Anyway, make sure the AHB is running at 24MHz
+				 * in low_bus_freq_mode.
+				 */
+				if (audio_bus_freq_mode)
+					imx_clk_set_rate(ahb_clk, LPAPM_CLK);
+				/*
 				 * Set DDR to 24MHz.
 				 * Since we are going to bypass PLL2,
 				 * we need to move ARM clk off PLL2_PFD2

osmc/vero-linux

(cherry picked from commit 680a8401aa030878cd4927dbba37a652a24655c6)
Signed-off-by: Matus Kral <matuskral@me.com>

arch/arm/mach-imx/busfreq-imx6.c
@@ -163,6 +163,12 @@ static void enter_lpm_imx6sl(void)
 		} else {
 			if (!ultra_low_bus_freq_mode && !low_bus_freq_mode) {
 				/*
+				 * Anyway, make sure the AHB is running at 24MHz
+				 * in low_bus_freq_mode.
+				 */
+				if (audio_bus_freq_mode)
+					clk_set_rate(ahb_clk, LPAPM_CLK);
+				/*
 				 * Set DDR to 24MHz.
 				 * Since we are going to bypass PLL2,
 				 * we need to move ARM clk off PLL2_PFD2